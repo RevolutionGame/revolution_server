85026cc1d25ac8128bd8e63b4dab6f4d9f62dd95
Joe <tug20864@temple.edu>
2018-11-15 14:36:03 -0500

    added credentials to keys and reffered to them from passport setup file

--- oauthConfig/keys.js
+++ oauthConfig/keys.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     google: {
-        clientID:'lskdjf',
-        clientSecret:'al;ksjfs',
+        clientID:'678432058435-i32ca9r52summcjtht1ams6a6j45davf.apps.googleusercontent.com',
+        clientSecret:'jSgM7xX3FkGKMgglERrc2MK8'
     }
 };
--- oauthConfig/passport_setup.js
+++ oauthConfig/passport_setup.js
@@ -4,12 +4,12 @@ const keys = require('./keys');
 
 
 passport.use(
-    new.GoogleStrategy(
+    new GoogleStrategy(
         {
             //options for google strat
-            clientID:'678432058435-i32ca9r52summcjtht1ams6a6j45davf.apps.googleusercontent.com',
-            clientSecret:'jSgM7xX3FkGKMgglERrc2MK8 '
+            clientID: keys.google.clientID,
+            clientSecret:keys.google.clientSecret
         }), () =>{
             //passport callback function
         }//end lambda
-)
+);
