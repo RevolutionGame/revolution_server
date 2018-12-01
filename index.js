d930161b0d26ddce9a49ca058e94c1c91e58f5f6
Joseph Wright <tug20864@temple.edu>
2018-12-01 12:36:41 -0500

    copied auth registers, routes and handlers in

--- index.js
+++ index.js
@@ -13,15 +13,101 @@ var routes = require('./routes');
 var models = require('./models');
 
 
-for (var route in routes) {
-    server.route(routes[route]);
-  }
   //server.connection({ routes: { cors: true } })
 
   var io = require('socket.io')(server.listener); 
   var controllerExportMethods = require('./controllers/game_controller');
   controllerExportMethods.socketInfo(server, io);
 //server start test
-  server.start(function(){
-    console.log("Server started");
+const start = async () => {
+
+  await server.register(  [   require('vision'), require('bell'), require('hapi-auth-cookie')  ] );
+
+  //server cookie strategy
+  server.auth.strategy('session', 'cookie', {
+    password: 'secret_cookie_encryption_password', //Use something more secure in production
+    redirectTo: '/auth/twitter', //If there is no session, redirect here
+    isSecure: false //Should be set to true (which is the default) in production
+});
+
+  //TWITTER AUTH STRATEGY
+  server.auth.strategy('twitter', 'bell', {
+    provider: 'twitter',
+    scope(request) {
+
+      const scopes = ['public_profile', 'email'];
+      if (request.query.wantsSharePermission) {
+        scopes.push('publish_actions');
+      }
+      return scopes;
+    },
+    config: {
+      getMethod: 'account/verify_credentials',
+      getParams: {include_email:'true' }//doesn't work without quotes!
+    },
+    password: 'cookie_encryption_password_secure',
+    clientId: 'U3scAkQRIkfxoZc9BhDcuJbb6',
+    clientSecret: 'HSsFGPF3mETMvPS27xQCf8fCw6mOtfx4oQ5ymbYJ1dVZM8fMmE',
+    isSecure: false     // Terrible idea but required if not using HTTPS especially if developing locally
+});
+
+//GOOGLE AUTH STRATEGY
+server.auth.strategy('google', 'bell', {
+  provider: 'google',
+  password: 'cookie_encryption_password_secure',
+  isSecure: false,
+  clientId: '678432058435-i32ca9r52summcjtht1ams6a6j45davf.apps.googleusercontent.com',
+  clientSecret: 'jSgM7xX3FkGKMgglERrc2MK8',
+  location: server.info.uri
+});
+  server.views({
+      engines: {
+          html: require('handlebars')
+      },
+      relativeTo: __dirname,
+      path: 'templates'
   });
+
+  server.route({
+      method: 'GET',
+      path: '/',
+      handler: function (request, h) {
+
+        console.log('testing3');
+          return h.view('home');
+      }
+  });
+
+    //Setup the routes (this could be done in an own file but for the sake of simplicity isn't)
+  
+    //NEED TO CHECK AGAINST DATABASE, IF USER NOT IN DATABASE
+    //ADD USER TO DATABASE AND LOGIN
+    server.route({
+      method: 'GET',
+      path: '/authed',
+      config: {
+        auth: {
+          strategy : 'session',
+          mode     : 'try'
+      }, //<-- require a session for this, so we have access to the twitter profile
+        handler: function(request, reply) {
+  
+          //Return a message using the information from the session
+          return ('Hello, ' + request.auth.credentials.email + '!');
+        }
+      }
+    });
+
+    //REGISTER ROUTES TO SERVER FROM ROUTES FOLDER
+    for (var route in routes) {
+      server.route(routes[route]);
+    }
+
+  server.start(function()
+{ 
+  console.log("Server started"); 
+});
+
+};
+
+start();
--- routes/player_routes.js
+++ routes/player_routes.js
@@ -20,6 +20,90 @@ module.exports = function() {
               //auth:'token',
               handler: playerController.create
             }
+          },
+        
+          //*****************NEW AUTH ADDON ROUTE **********************/
+
+          {
+            method: '*',
+            path: '/auth/twitter',
+            config : {
+              tags        : ['user', 'auth', 'session', 'login'],
+              description : 'Begin a user session.',
+              auth        : {
+                  strategy : 'twitter',
+                  mode     : 'try'
+              },
+      
+              handler: function(request, reply) {
+        
+                if (!request.auth.isAuthenticated) {
+                  return ('Authentication failed: ' + request.auth.error.message);
+                }
+        
+                //Just store a part of the twitter profile information in the session as an example. You could do something
+                //more useful here - like loading or setting up an account (social signup).
+                const profile = request.auth.credentials.profile;
+                //log out profile to see what is returned
+                console.log(profile);
+
+                //CANT SEEM TO SET VALUES HERE, WHEN TRY TO ACCESS VALUES ARE UNDEFINED!!!
+                //HAVE BEEN JUST PASSING REQUEST AND ACCESSING VALUES DIRECTLY
+                request.cookieAuth.set({
+                  twitterId: request.auth.credentials.profile.id,
+                  username: profile.displayName,
+                  displayName: profile.displayName,
+                  name: profile.raw.name,
+                  email: profile.raw.email
+                });
+      
+                console.log('checking oauth session:  ' + request.auth.credentials.twitterId);
+
+                //redirect to login
+                return playerController.authLogin(request, reply);
+              }
+            }
+          },
+
+          {
+            method: '*',
+            path: '/auth/google',
+            config : {
+              tags        : ['user', 'auth', 'session', 'login'],
+              description : 'Begin a user session.',
+              auth        : {
+                  strategy : 'google',
+                  mode     : 'try'
+              },
+      
+              handler: function(request, reply) {
+        
+                if (!request.auth.isAuthenticated) {
+                  return ('Authentication failed: ' + request.auth.error.message);
+                }
+        
+                //Just store a part of the twitter profile information in the session as an example. You could do something
+                //more useful here - like loading or setting up an account (social signup).
+                const profile = request.auth.credentials.profile;
+                
+                //log out profile to see what is returned
+                console.log(profile);
+
+                //CANT SEEM TO SET VALUES HERE, WHEN TRY TO ACCESS VALUES ARE UNDEFINED!!!
+                //HAVE BEEN JUST PASSING REQUEST AND ACCESSING VALUES DIRECTLY
+                request.cookieAuth.set({
+                  googleId: request.auth.credentials.profile.id,
+                  username: profile.displayName,
+                  displayName: profile.displayName,
+                  name: profile.raw.name,
+                  email: profile.raw.email
+                });
+
+                //redirect to login
+                return playerController.authLogin(request, reply);
+              }
+            }
           }
+
         ];
     }();
